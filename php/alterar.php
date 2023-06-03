604eee9ab6bf98f23676053719c128a997f8ee3b
rosilanydias <132942248+rosilanydias@users.noreply.github.com>
2023-06-03 17:35:37 -0300

    Add files via upload

---
+++ php/alterar.php
@@ -0,0 +1,30 @@
+<?php 
+
+//Incluir a conexão
+include("conexao.php");
+
+//Obter dados
+$obterDados = file_get_contents("php://input");
+
+//Extrair os dados do JSON
+$extrair = json_decode($obterDados);
+
+//Separar os dados do JSON
+$idCurso = $extrair->idCurso;
+$nomeCurso = $extrair->nomeCurso;
+$valorCurso = $extrair->valorCurso;
+
+//SQL
+$sql = "UPDATE cursos SET nomeCurso='$nomeCurso', valorCurso=$valorCurso WHERE idCurso=$idCurso";
+mysqli_query($conexao, $sql);
+
+//Exportar dados cadastrados
+$curso = [
+    'idCurso' => $idCurso,
+    'nomeCurso' => $nomeCurso,
+    'valorCurso' => $valorCurso
+];
+
+json_encode(['curso' => $curso]);
+
+?>
